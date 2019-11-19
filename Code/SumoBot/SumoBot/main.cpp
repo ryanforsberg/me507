c000ea4f3162e3fe78665019fdf2961393f46ce5
ryanforsberg <56706742+ryanforsberg@users.noreply.github.com>
2019-11-19 10:49:25 -0800

    Continued ADC Reading development

--- Code/SumoBot/SumoBot/main.cpp
+++ Code/SumoBot/SumoBot/main.cpp
@@ -17,19 +17,57 @@ int16_t ReadADC(uint8_t Channel)
 		ADCA.PRESCALER = ADC_PRESCALER_DIV256_gc;  // ADC Frequency = 
 	}
 	ADCA.CH0.CTRL = ADC_CH_INPUTMODE_SINGLEENDED_gc | ADC_CH_GAIN_1X_gc; // Set single ended ADC input mode and gain of 1
+	
+	
+	
+	
 }
 
 
 uint8_t Edge_Detection()
 {
 	// Setup Edge Detection
+	edgeAchan = 
+	edgeBchan = 
+	edgeCchan =
+	edgeDchan =
+	edgeEchan =
+	edgeFchan =
+	edge_threshold = 
 	
 	for(;;)
 	{
-		ReadADC()
+		if (ReadADC(edgeAchan) > edge_threshold)
+		{
+			edge_out = 1;
+		}
 		
+		if (ReadADC(edgeBchan) > edge_threshold)
+		{
+			edge_out = edge_out | (1<<1);
+		}
 		
+		if (ReadADC(edgeCchan) > edge_threshold)
+		{
+			edge_out = edge_out | (1<<2);
+		}
 		
+		if (ReadADC(edgeDchan) > edge_threshold)
+		{
+			edge_out = edge_out | (1<<3);
+		}
+		
+		if (ReadADC(edgeEchan) > edge_threshold)
+		{
+			edge_out = edge_out | (1<<4);
+		}
+		
+		if (ReadADC(edgeFchan) > edge_threshold)
+		{
+			edge_out = edge_out | (1<<5);
+		}
+		
+		return edge_out;
 		vTaskDelay();
 	}
 }
