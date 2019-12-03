30ecaa4f9ab26714668220b9aa649fed0528e603
phabets <57044454+phabets@users.noreply.github.com>
2019-12-02 20:07:23 -0800

    Added shares.h, motor driver, and location tasks

---
+++ Code/SumoBot/shares.h
@@ -0,0 +1,13 @@
+﻿// Communication variables file
+
+// Motor driver command communications
+extern shared_data<bool>* commForward;
+extern shared_data<bool>* commBackward;
+extern shared_data<bool>* commLeft;
+extern shared_data<bool>* commRight;
+
+// Navigation data share
+extern shared_data<uint16_t>* pos_X; // Global X
+extern shared_data<uint16_t>* pos_Y; // Global Y
+extern shared_data<uint16_t>* pos_T; // Global Theta (Heading), measured positive CCW from the X axis
+									 // Origin is in the center of the ring
